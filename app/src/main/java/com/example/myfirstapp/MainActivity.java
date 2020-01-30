e4349d26538f6598b4013759819bb07a438e1ac6
brutekoon <brutekoon@gmail.com>
2020-01-30 16:13:22 +0900

    New Module for Text View and practice View Component

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.mifirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, mesage);
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
---
+++ viewexam/src/main/java/com/example/viewexam/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.viewexam;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onButtonClicked(View view){
+
+
+    }
+    public void onVisibleClicked(View view){
+
+
+    }
+    public void onNoVisibleClicked(View view){
+
+
+    }
+}
