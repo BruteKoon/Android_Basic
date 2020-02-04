d812eb8401451c4be22f00175b29147dbe520155
brutekoon <brutekoon@gmail.com>
2020-02-04 23:48:58 +0900

    BroadcastReceiver Practice(using manifest)

---
+++ broadcastrecieverexam/src/main/java/com/example/broadcastrecieverexam/MyReceiver.java
@@ -0,0 +1,17 @@
+package com.example.broadcastrecieverexam;
+
+        import android.content.BroadcastReceiver;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    private static final String MY_ACTION = "com.example.broadcastreceiverexam.action.ACTION_BATTERY_CHANGED";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String actionName = intent.getAction();
+        Toast.makeText(context, "받은 액션 : "+actionName , Toast.LENGTH_SHORT).show();
+    }
+}
