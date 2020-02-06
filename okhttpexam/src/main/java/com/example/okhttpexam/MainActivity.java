1feb0de012c3932a443b6ee76a3133f8972c6648
brutekoon <brutekoon@gmail.com>
2020-02-06 13:29:46 +0900

    Merge branch 'master' of https://github.com/BruteKoon/Android_Basic
    # Conflicts:
    #	settings.gradle

--- okhttpexam/src/main/java/com/example/okhttpexam/MainActivity.java
+++ okhttpexam/src/main/java/com/example/okhttpexam/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.okhttpexam;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
 
